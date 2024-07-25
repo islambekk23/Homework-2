51a447a702bd9d8ec929ef142c1a0e594cb28370
islambekk23 <islambekkunabaev@gmail.com>
2024-07-25 23:27:21 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,5 @@
+course_info = 'Курсж: Python,'
+homework_count = 'всего задач:12,'
+hours_spent = 'затрачено часов:1.5,'
+average_time_per_task = 'среднее время выполнения 0.125 часа.'
+print(course_info,homework_count,hours_spent,average_time_per_task)
